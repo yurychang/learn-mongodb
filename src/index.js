036a57fe546cd53fa39ce0de47df922e04998a36
Yury <yuury.chang@gmail.com>
2023-04-18 03:06:10 +0800

    d

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
 const mongoose = require('mongoose');
-mongoose
-    .connect('mongodb://127.0.0.1:27017/test')
-    .then(() => console.log('資料庫連接成功'));
+
+const connURL = 'mongodb://127.0.0.1:27017/';
+
+const MyCollection = mongoose.model(
+    'myCollection',
+    new mongoose.Schema(
+        { a: Number, b: Number },
+        { strict: false, collection: 'myCollection' }
+    )
+);
+
+async function main() {
+    try {
+        await mongoose.connect(connURL);
+
+        await MyCollection.deleteMany({});
+        await MyCollection.insertMany([
+            { a: 1, b: 1 },
+            { a: 1, b: 1 },
+        ]);
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+main();
